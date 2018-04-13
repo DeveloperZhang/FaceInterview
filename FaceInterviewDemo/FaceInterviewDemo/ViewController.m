2096011864310f7d8a00e858cd0f5d6d8a989a05
Zhang Yu <351045731@qq.com>
2018-04-13 08:38:09 +0800

    init

---
+++ FaceInterviewDemo/FaceInterviewDemo/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  FaceInterviewDemo
+//
+//  Created by Vicent on 2018/4/13.
+//  Copyright © 2018年 VicentZ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+/* 数据源 Json数据: [{"title":"标题1","subTitle":"子标题1"},{"title":"标题2","subTitle":"子标题2"}......]
+   可以硬编码初始化写死在dataSource中
+*/
+@property (nonatomic, strong) NSMutableArray *dataSource;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
